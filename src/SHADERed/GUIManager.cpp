6ae05c7f71953454f39847bec3dfe4c179ad64ac
dfranx <dfranx00@gmail.com>
2020-08-24 02:14:18 +0200

    Bump version

--- src/SHADERed/GUIManager.cpp
+++ src/SHADERed/GUIManager.cpp
@@ -1596,7 +1596,7 @@ namespace ed {
 		ImGui::SetNextWindowSize(ImVec2(Settings::Instance().CalculateSize(270), Settings::Instance().CalculateSize(220)), ImGuiCond_Always);
 		if (ImGui::BeginPopupModal("About##main_about", 0, ImGuiWindowFlags_NoResize)) {
 			ImGui::TextWrapped("(C) 2020 dfranx");
-			ImGui::TextWrapped("Version 1.3.6");
+			ImGui::TextWrapped("Version 1.4");
 			ImGui::TextWrapped("Internal version: %d", WebAPI::InternalVersion);
 			ImGui::NewLine();
 			UIHelper::Markdown("This app is open sourced: [link](https://www.github.com/dfranx/SHADERed)");
--- src/SHADERed/Objects/WebAPI.h
+++ src/SHADERed/Objects/WebAPI.h
@@ -7,7 +7,7 @@ namespace ed {
 	class WebAPI {
 	public:
 		static const std::string URL;
-		static const int InternalVersion = 15;
+		static const int InternalVersion = 16;
 
 		// info that /api/search will return
 		struct ShaderResult 
