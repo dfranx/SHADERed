b0857a0a67c8ea99b178ca0dd273525df2062eec
dfranx <dfranx00@gmail.com>
2020-10-20 16:10:45 +0200

    SPIR-V syntax highlighting & make 'Find' case insensitive

--- src/SHADERed/UI/Tools/StatsPage.cpp
+++ src/SHADERed/UI/Tools/StatsPage.cpp
@@ -27,52 +27,40 @@ namespace ed {
 	void StatsPage::Refresh(PipelineItem* item, ShaderStage stage)
 	{
 		m_spv.clear();
+		std::string disassembly = "";
+
+		spvtools::SpirvTools core(SPV_ENV_UNIVERSAL_1_3);
 
 		if (item->Type == PipelineItem::ItemType::ShaderPass) {
 			pipe::ShaderPass* pass = (pipe::ShaderPass*)item->Data;
-
-			spvtools::SpirvTools core(SPV_ENV_UNIVERSAL_1_3);
-			std::string disassembly;
 
 			m_spv = pass->VSSPV;
 			if (stage == ShaderStage::Pixel)
 				m_spv = pass->PSSPV;
 			else if (stage == ShaderStage::Geometry)
 				m_spv = pass->GSSPV;
-
-			core.Disassemble(m_spv, &disassembly, SPV_BINARY_TO_TEXT_OPTION_INDENT | SPV_BINARY_TO_TEXT_OPTION_FRIENDLY_NAMES);
-
-			m_spirv.SetPalette(ThemeContainer::Instance().GetTextEditorStyle(Settings::Instance().Theme));
-			m_spirv.SetText(disassembly);
 		}
 		else if (item->Type == PipelineItem::ItemType::ComputePass) {
 			pipe::ComputePass* pass = (pipe::ComputePass*)item->Data;
 
-			spvtools::SpirvTools core(SPV_ENV_UNIVERSAL_1_3);
-			std::string disassembly;
-
 			m_spv = pass->SPV;
-			core.Disassemble(m_spv, &disassembly, SPV_BINARY_TO_TEXT_OPTION_INDENT | SPV_BINARY_TO_TEXT_OPTION_FRIENDLY_NAMES);
-
-			m_spirv.SetPalette(ThemeContainer::Instance().GetTextEditorStyle(Settings::Instance().Theme));
-			m_spirv.SetText(disassembly);
 		} else if (item->Type == PipelineItem::ItemType::PluginItem) {
 			pipe::PluginItemData* pass = (pipe::PluginItemData*)item->Data;
-
-			spvtools::SpirvTools core(SPV_ENV_UNIVERSAL_1_3);
-			std::string disassembly;
 
 			unsigned int spvSize = pass->Owner->PipelineItem_GetSPIRVSize(pass->Type, pass->PluginData, (ed::plugin::ShaderStage)stage);
 			unsigned int* spv = pass->Owner->PipelineItem_GetSPIRV(pass->Type, pass->PluginData, (ed::plugin::ShaderStage)stage); 
 
 			m_spv = std::vector<unsigned int>(spv, spv + spvSize);
-			core.Disassemble(m_spv, &disassembly, SPV_BINARY_TO_TEXT_OPTION_INDENT | SPV_BINARY_TO_TEXT_OPTION_FRIENDLY_NAMES);
-
-			m_spirv.SetPalette(ThemeContainer::Instance().GetTextEditorStyle(Settings::Instance().Theme));
-			m_spirv.SetText(disassembly);
 		}
 
-		if (!m_spv.empty())
+		if (!m_spv.empty()) {
+			core.Disassemble(m_spv, &disassembly, SPV_BINARY_TO_TEXT_OPTION_INDENT | SPV_BINARY_TO_TEXT_OPTION_FRIENDLY_NAMES);
+
 			m_info.Parse(m_spv);
+		}
+
+		m_spirv.SetPalette(ThemeContainer::Instance().GetTextEditorStyle(Settings::Instance().Theme));
+		m_spirv.SetLanguageDefinition(TextEditor::LanguageDefinition::SPIRV());
+		m_spirv.SetText(disassembly);
 	}
 }
