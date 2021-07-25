ae1ce8c09f4632ed8df41ab0ee090fb0c3584a5b
Alain Greppin <agreppin@chilibi.ch>
2021-07-25 09:00:29 +0200

    PR#232 followup: don't flip imported model UVs

--- src/SHADERed/Engine/Model.cpp
+++ src/SHADERed/Engine/Model.cpp
@@ -78,7 +78,7 @@ namespace ed {
 
 			// read file via ASSIMP
 			Assimp::Importer importer;
-			const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+			const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate);
 
 			// check for errors
 			if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
